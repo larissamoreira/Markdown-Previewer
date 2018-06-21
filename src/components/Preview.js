2c965a09e7ba4a7d547148c2dcdc9d2a6143b8d0
Larissa Moreira <larissamgusmao@gmail.com>
2018-06-20 21:11:00 -0300

    Creating the markup

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,9 +2,12 @@ import React from "react";
 var myMarked = require("marked");
 
 export default class Preview extends React.Component {
+  createMarkup(textMarked) {
+    return { __html: textMarked };
+  }
   render() {
     const { text } = this.props;
     const textMarked = myMarked(text);
-    return <div id="preview">{textMarked}</div>;
+    return <div dangerouslySetInnerHTML={this.createMarkup(textMarked)} />;
   }
 }
