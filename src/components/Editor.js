f735474e3cd691750b0f314b013c8ca7b34455be
Larissa Moreira <larissamgusmao@gmail.com>
2018-06-21 20:31:37 -0300

    Basic style

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Preview from "./Preview";
+import "../styles.css";
 
 export default class Editor extends React.Component {
   constructor(props) {
@@ -16,16 +17,52 @@ export default class Editor extends React.Component {
   };
 
   componentDidMount() {
-    const defaultText =
-      " # H1\n" +
-      "## H2\n" +
-      "[here is a link](https://www.google.com)\n" +
-      "`back-ticks around`\n" +
-      "``` print('codeblock') ```\n" +
-      "1. First ordered list item\n" +
-      "> Block Quotes!\n" +
-      "![React Logo w/ Text](https://goo.gl/Umyytc)" +
-      "**bolded text**";
+    const defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
 
     this.setState({
       input: defaultText
@@ -34,14 +71,15 @@ export default class Editor extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
+      <div className="wrapper">
         <textarea
           id="editor"
           value={this.state.input}
           onChange={this.handleChange}
+          rows="15"
         />
         <Preview text={this.state.input} />
-      </React.Fragment>
+      </div>
     );
   }
 }
